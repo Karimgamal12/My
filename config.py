49d10435dde7e21ca87c7c7aeba70d02dec44703
Karimgamal12 <132415145+Karimgamal12@users.noreply.github.com>
2023-05-13 03:22:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,27 +5,27 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578")) #لا تغير هاذة القيمة
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")#لا تغير هاذة القيمة
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-SESSION_NAME = getenv("SESSION_NAME", "")
+API_ID = int(getenv("API_ID", "11371179")) #لا تغير هاذة القيمة
+API_HASH = getenv("API_HASH", "9ba892aa7ac568ab8094f609f8dca656")#لا تغير هاذة القيمة
+BOT_TOKEN = getenv("BOT_TOKEN", "5969211519:AAHafRveNCNdmBgwE6EdvAySmVqEUWLPXyg")
+SESSION_NAME = getenv("SESSION_NAME", "BAABGwYLNcX6Pum-ZMjwWjLJALxufxJ-U0a9qkHjybgnBsmn5WzD1T5OiKWsRQmt_rOMB6lb-nCKTiGPl-wyr_cZSRF6kUoz_-Uumgqe8itKvYML-B7mecCrpaMe1WnG2uHrpMWUoTwkrH_Mioeo_pj8cuRe_3peXrHvpl1H2oLzQDJbi1koR11ZDLi7LhX3juWGK3CffK-7J-j3eRIaAXzrg2N9G2VdVLvaZ1hHD07Faecum1_t0-OMHMVyIkeGpe0kDl3GQaUZQAtFmFSHmstB-rtWOHzTFE42mq5vPPa_QtVDXKTUb4zBSBbqTt2Jxcdom7aZoTMy1EeZ_EB8kd2KAAAAAU1r5qoABAABGwYLNcX6Pum-ZMjwWjLJALxufxJ-U0a9qkHjybgnBsmn5WzD1T5OiKWsRQmt_rOMB6lb-nCKTiGPl-wyr_cZSRF6kUoz_-Uumgqe8itKvYML-B7mecCrpaMe1WnG2uHrpMWUoTwkrH_Mioeo_pj8cuRe_3peXrHvpl1H2oLzQDJbi1koR11ZDLi7LhX3juWGK3CffK-7J-j3eRIaAXzrg2N9G2VdVLvaZ1hHD07Faecum1_t0-OMHMVyIkeGpe0kDl3GQaUZQAtFmFSHmstB-rtWOHzTFE42mq5vPPa_QtVDXKTUb4zBSBbqTt2Jxcdom7aZoTMy1EeZ_EB8kd2KAAAAAU1r5qoA")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9") # @ هنا ضع يوزر حسابك بدون 
-ALIVE_NAME = getenv("ALIVE_NAME", "sonng") # هنا ضع اسم حسابك
-BOT_USERNAME = getenv("BOT_USERNAME", "") # @ هنا ضع يوزر البوت بدون 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR") 
+OWNER_USERNAME = getenv("OWNER_USERNAME", "e1_x_z") # @ هنا ضع يوزر حسابك بدون 
+ALIVE_NAME = getenv("ALIVE_NAME", "Dev monzer") # هنا ضع اسم حسابك
+BOT_USERNAME = getenv("BOT_USERNAME", "XDEV_MEMO_BOT") # @ هنا ضع يوزر البوت بدون 
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/rRRR") 
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main") #لا تغير هاذة القيمة
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60")) #لا تغير هاذة القيمة
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9") # @ هنا ضغ يوزر كروبك بدون 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444") # @ هنا ضغ يوزر قناتك بدون
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "UUX_VVX") # @ هنا ضغ يوزر كروبك بدون
+
+
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "UUU_iiiiii") # @ هنا ضغ يوزر قناتك بدون
 
 # database, decorators, handlers mandatory vars
-MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
+MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.aPVu_9gtaPs.mongodb.net/veez?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
-                                             #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5593884330").split(SUDO_USERS = list(map(int, getenv("SUDO_USERS"," 5593884330").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
